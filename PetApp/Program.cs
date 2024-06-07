2d34aa6576f01b8fee916bc52293ceaebc239e2e
Merlin George <Mgeorge9980@conestogac.on.ca>
2024-06-07 18:27:53 -0400

    * Two options given in the main menu implemented - Feed and status check

--- PetApp/Program.cs
+++ PetApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -10,8 +12,9 @@ namespace PetApp
     internal class PetLogic
     {
         static String petName;
-        static int actionSelected;
-        Pet pet;
+        static String actionSelected;
+        static bool initialStatus = true;
+        static Pet pet;
 
         static void Main(string[] args)
         {
@@ -21,94 +24,117 @@ namespace PetApp
             askPetType();
 
             // Asking users choice for pet name and display a welcome message for pet.
-            petName=askPetName();
+            askPetName();
 
+            Console.WriteLine($"\n {petName}'s status \n\n\tHunger = 5 , \t Health =  5 , \t Happiness = 5");
+            
             //Display the menu that include the options to interact with the pet and perform actions 
-            actionSelected = displayMainMenu();
+            displayMainMenu();
 
-            //Perform the actions based on the option selected by the user
-            performAction(petName, actionSelected);
         }
 
         static void askPetType()
         {
-            int choice = 0;
-            Console.WriteLine("\nPlease choose a pet \n\t1. Cat\n\t2. Dog \n\t3. Rabbit");
-            choice = int.Parse(Console.ReadLine());
-            Console.WriteLine($"User Input: {choice}");
-
-            switch (choice)
+            Console.Write("\nPlease choose a pet \n\t1. Cat\n\t2. Dog \n\t3. Rabbit\n");
+            Console.Write("User Input: ");
+            String choice = Console.ReadLine();
+            if (choice != null)
             {
-                case 1:
-                    Console.WriteLine("\nYou've chosen a Cat. What would you like to name your pet?");
-                    break;
-                case 2:
-                    Console.WriteLine("\nYou've chosen a Dog. What would you like to name your pet?");
-                    break;
-                case 3:
-                    Console.WriteLine("\nYou've chosen a Rabbit. What would you like to name your pet?");
-                    break;
-                default:
-                    Console.WriteLine("\nInvalid choice. Kindly enter a number between 1 and 3");
-                    askPetType();
-                    break;
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("\nYou've chosen a Cat. What would you like to name your pet?");
+                        break;
+                    case "2":
+                        Console.WriteLine("\nYou've chosen a Dog. What would you like to name your pet?");
+                        break;
+                    case "3":
+                        Console.WriteLine("\nYou've chosen a Rabbit. What would you like to name your pet?");
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid choice. Kindly enter a number between 1 and 3");
+                        askPetType();
+                        break;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid choice. Kindly enter a number between 1 and 3");
+                askPetType();
             }
         }
 
 
-        static string askPetName()
+        static void askPetName()
         {
             Console.WriteLine("\nEnter the pet name");
             petName = Console.ReadLine();
+            pet = new Pet(petName);
             Console.WriteLine($"\nWelcome, {petName}! Let's take a good care of him.\n");
-            return petName;
         }
 
-        static int displayMainMenu()
+        static void displayMainMenu()
         {
             //Options in the MainMenu
-            Console.WriteLine("\n\tMain Menu:\n");
+            Console.WriteLine("\nMainMenu options\n");
             Console.WriteLine("\t1. Feed Buddy");
             Console.WriteLine("\t2. Play with Buddy");
             Console.WriteLine("\t3. Let Buddy Rest");
             Console.WriteLine("\t4. Check Buddy's Status");
             Console.WriteLine("\t5. Exit");
-            Console.WriteLine("\n\tSelect an option \n");
+            Console.WriteLine("\nPlease choose an option\n");
 
-            return int.Parse(Console.ReadLine());
+            Console.Write("User Input: ");
+            actionSelected = Console.ReadLine();
+
+            //Perform the actions based on the option selected by the user
+            PetLogic petLogic = new PetLogic();
+            petLogic.performAction(petName, actionSelected);
+
         }
 
-        static void performAction(string name, int action)
+        public void performAction(string name, string action)
         {
-            Console.WriteLine("User Input:" +action);
 
-            switch (action)
+            if (action != null)
             {
-                case 1:
-                    Console.WriteLine("inside switch statement");
-                    Console.WriteLine($"Inside case 1: petname {name}, Action : " + action);
-              
-                    break;
+                switch (action)
+                {
+                    case "1":
+                        pet.feed();
+                        displayMainMenu();
+                        break;
 
-                case 2:
-                    //call play method
-                    break;
-                case 3:
-                    //call rest method
-                    break;
-                case 4:
-                    //call status method
-                    break;
-                case 5:
-                    // Exit from the app
-                    Console.WriteLine("Exiting the application, GoodBye !");
-                    break;
+                    case "2":
+                        //call play method
+                        //displayMainMenu();
+                        break;
 
-                default:
-                    Console.WriteLine("Invalid Option. Please try again");
-                    break;
+                    case "3":
+                        //call rest method
+                       // displayMainMenu();
+                        break;
+
+                    case "4":
+                        pet.printStatus();
+                        //displayMainMenu();
+                        break;
+
+                    case "5":
+                        // Exit from the app
+                        Console.WriteLine("Exiting the application, GoodBye !");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Option. Please try again");
+                        break;
+                }
             }
-            Console.WriteLine("outside switch statement");
+            else
+            {
+                Console.WriteLine("Invalid Option. Please choose a valid option between 1 and 5");
+            }
 
         }
     }
@@ -117,7 +143,72 @@ namespace PetApp
 
     public class Pet
     {
+        public string name;
+        public int hunger;
+        public int happiness;
+        public int health;
+        public bool initialStatus;
 
+        public Pet(string name)
+        {
+            this.name = name;
+            this.hunger = 5;
+            this.happiness = 5;
+            this.health = 5;
+        }
+
+        public void feed()
+        {
+
+            if (hunger > 0 && hunger < 10)
+            {
+                hunger -= 1;
+
+                if (health >=0 && health < 10)
+                {
+                    ++health;
+                }
+                
+                Console.WriteLine($"\t You fed {name}. His hunger decreases, and health improves slightly.");
+                initialStatus = false;
+                printStatus();
+            }
+            else
+            {
+                Console.WriteLine($"{name} is already full and not hungry");
+            }
+
+        }
+
+        public void play()
+        {
+
+            if (hunger > 0 && hunger < 10)
+            {
+                hunger = hunger+1;
+
+                if (health >= 0 && health < 10)
+                {
+                    ++health;
+                }
+
+                Console.WriteLine($"\t You fed {name}. His hunger decreases, and health improves slightly.");
+                initialStatus = false;
+                printStatus();
+            }
+            else
+            {
+                Console.WriteLine($"{name} is already full and not hungry");
+            }
+
+        }
+
+
+        public void printStatus()
+        {
+                Console.WriteLine($"\n {name}'s status \n\nHunger = {hunger} , \t Health =  {health} , \t Happiness = {happiness}");
+            
+        }
     }
 
 }
