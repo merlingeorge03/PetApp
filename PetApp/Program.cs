80b7057016b6dabe51dd794ce0bf3fa4fddbc0db
Merlin George <Mgeorge9980@conestogac.on.ca>
2024-06-07 12:00:42 -0400

    Add project files.

---
+++ PetApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetApp
+{
+    internal class PetLogic
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWelcome to Virtual Pet App");
+        }
+    }
+
+    public class Pet
+    {
+
+    }
+
+}
